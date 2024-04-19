8fd44bcd02d7784094d3c1e860c8ac813d1b1c72
TicklerXD <167575102+TicklerXD@users.noreply.github.com>
2024-04-19 23:13:09 +0300

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using MelonLoader;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("SaikoCameraMod")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SaikoCameraMod")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: MelonInfo(typeof(SaikoCameraMod.SaikoCamera), "SaikoCameraMod", "1.0.0.0", "Tickler")]
+[assembly: MelonGame("Habupain", "Saiko no sutoka")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("6b8ebd49-c8e2-403f-ad66-1d77316c92ea")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SaikoCamera.cs
@@ -0,0 +1,71 @@
+﻿using MelonLoader;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+// *********************** INFO FOR MOD DEVELOPERS ********************
+//
+//        If you decide to use the code of my mod as a template for your mods, then you need to copy and paste the following lines in Properties/AssemblyInfo.cs:
+
+//        [assembly: MelonInfo(typeof(*Your plugin class (SaikoCameraMod.SaikoCamera in my case)*), "*Name of your mode*", "*Any version*", "*Your nickname*")]
+//        [assembly: MelonGame("*Game Developer*", "*Name of game*")] -----> You can see this information from the MelonLoader console when you start the game
+//
+//*********************************************************************
+
+namespace SaikoCameraMod
+{
+    public class SaikoCamera : MelonMod
+    {
+        static string playText = string.Empty; // Set the "Play" text for Nightmare Mode
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            base.OnSceneWasLoaded(buildIndex, sceneName);
+
+            if (!sceneName.Contains("LevelNew")) return;
+
+            //Set player's camera
+            var mainCam = Camera.main;
+            mainCam.rect = new Rect(.75f, 0.75f, .35f, .35f);
+            mainCam.depth = 5;
+
+            if (GameObject.Find("bunny_saiko") || GameObject.Find("sane saiko") || GameObject.Find("nightmare"))
+            {
+                var cam = new GameObject("Camera");
+                Transform head;
+                if (GameObject.Find("bunny_saiko")) head = GetHead("bunny_saiko");
+                else if (GameObject.Find("nightmare")) head = GetHead("nightmare");
+                else head = GetHead("sane saiko");
+
+                // Creating Saiko's camera
+                cam.AddComponent<Camera>();
+                cam.transform.position = head.position;
+                cam.transform.rotation = head.rotation;  
+                cam.transform.SetParent(head);
+                cam.SetActive(true);
+
+                return;
+            }
+
+            var cam2 = GetHead("yandere 2").FindChild("Camera");
+            cam2.gameObject.SetActive(true);
+        }
+        public override void OnUpdate()
+        {
+            base.OnUpdate();
+
+            // Just unlocks the Nightmare Mode
+            if (SceneManager.GetActiveScene().name.Contains("MainMenu"))
+            {
+                var n = GameObject.Find("MainMenu").transform.FindChild("Canvas").FindChild("ChooseDifficulty").FindChild("Practice (1)").gameObject;
+                var textComp = n.transform.FindChild("Text").GetComponent<Text>();
+                if (playText.Equals(string.Empty)) playText = textComp.text;
+
+                textComp.text = playText;
+                n.GetComponent<Button>().interactable = true;
+            }
+        }
+        Transform GetHead(string name) =>
+            GameObject.Find(name).transform.FindChild("Armature" + (name.Contains("bunny") ? ".002" : "")).FindChild("mixamorig:Hips").FindChild("mixamorig:Spine").FindChild("mixamorig:Spine1")
+                .FindChild("mixamorig:Spine2").FindChild("mixamorig:Neck").FindChild("mixamorig:Head");
+    }  
+}
